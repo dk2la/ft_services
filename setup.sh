66d6de2b46c90161971c0fe4bad4d4a69ba81a44
Shonna Jakku <sjakku@si-k2.kzn.21-school.ru>
2020-10-24 22:54:07 +0300

    test;

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-minikube start
+minikube start --vm-driver=virtualbox
+# minikube start
 
-eval $(minikube docker-env)
+eval $(minikube docker-env) > /dev/null
+
+minikube addons enable metallb
+kubectl apply -f ./srcs/metallb.yaml
 #build nginx
 docker build -t nginx-service ./srcs/nginx/
 kubectl apply -f ./srcs/nginx.yaml
